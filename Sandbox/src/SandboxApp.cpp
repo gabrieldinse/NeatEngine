54bc02a61b8ac25f5541ccb2645688baf92bde13
Gabriel Dinse <gabriel.dinse@outlook.com>
2025-05-23 07:42:55 -0300

    Removed unnecessary includes

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -73,21 +73,21 @@ public:
 
    virtual void imGuiRender() override
    {
-      //ImGui::Begin("Settings");
+      ImGui::Begin("Settings");
 
       auto stats = Neat::Renderer2D::getStats();
-      //ImGui::Text("Renderer2D stats:");
-      //ImGui::Text("Draw calls: %d", stats.drawCalls);
-      //ImGui::Text("Quads: %d", stats.quadCount);
-      //ImGui::Text("Indexes: %d", stats.getTotalIndexCount());
-      //ImGui::Text("Vertexes: %d\n", stats.getTotalVertexCount());
+      ImGui::Text("Renderer2D stats:");
+      ImGui::Text("Draw calls: %d", stats.drawCalls);
+      ImGui::Text("Quads: %d", stats.quadCount);
+      ImGui::Text("Indexes: %d", stats.getTotalIndexCount());
+      ImGui::Text("Vertexes: %d\n", stats.getTotalVertexCount());
 
-      //ImGui::SliderInt("Number of columns", &this->numberOfColumns, 0, 500);
-      //ImGui::SliderInt("Number of lines", &this->numberOfLines, 0, 500);
+      ImGui::SliderInt("Number of columns", &this->numberOfColumns, 0, 500);
+      ImGui::SliderInt("Number of lines", &this->numberOfLines, 0, 500);
 
-      //ImGui::ColorEdit4("Square Color", this->tint.dataPointer());
+      ImGui::ColorEdit4("Square Color", this->tint.dataPointer());
 
-      //ImGui::End();
+      ImGui::End();
    }
 
 private:
