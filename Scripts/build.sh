f10ea7704d5cc6e933c68ce98e92ead0963c76b5
Gabriel Dinse <gabriel.dinse@outlook.com>
2025-08-08 10:47:37 -0300

    Removing cache every time the build script runs; Fixed bin directory not containg the target name prefix

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,6 +2,7 @@
 readonly build_target=${1:-Debug}
 readonly target=${1:-all}
 
-cmake -S. -B ./build
+rm build/CMakeCache.txt
+cmake -S. -B ./build -DCMAKE_BUILD_TYPE="${build_target}"
 cmake --build ./build --config "${build_target}" --target "${target}" -j 6 --
 
