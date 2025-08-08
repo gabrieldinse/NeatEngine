5ce59fd65ed664223d2f36691fe3a2772933dbad
Gabriel Dinse <gabriel.dinse@outlook.com>
2025-08-08 19:40:41 -0300

    Minor changes on build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,7 +2,15 @@
 readonly build_target=${1:-Debug}
 readonly target=${1:-all}
 
-rm build/CMakeCache.txt
-cmake -S. -B ./build -DCMAKE_BUILD_TYPE="${build_target}"
-cmake --build ./build --config "${build_target}" --target "${target}" -j 6 --
+readonly curr_dir="$(basename "$(pwd)")"
+
+if [[ "${curr_dir}" == "build" ]]; then
+	build_dir="."
+else
+	build_dir="./build"
+fi
+
+rm "${build_dir}/CMakeCache.txt"
+cmake -S. -B "${build_dir}" -DCMAKE_BUILD_TYPE="${build_target}"
+cmake --build "${build_dir}" --config "${build_target}" --target "${target}" -j 6 --
 
