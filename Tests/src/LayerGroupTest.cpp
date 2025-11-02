e69f33f5d7177db8faa2b582b3d54050f5615c4c
Gabriel Dinse <gabriel.dinse@outlook.com>
2025-11-02 09:35:59 -0300

    Added LayerGroup pop layers and disable/enable layers tests

--- Tests/src/LayerGroupTest.cpp
+++ Tests/src/LayerGroupTest.cpp
@@ -9,8 +9,14 @@ TEST(LayerGroupTest, PushLayers) {
   EXPECT_EQ(layerGroup.size(), 0);
 
   layerGroup.pushLayer<TestingLayer>();
+  EXPECT_EQ(layerGroup.size(), 1);
+
   layerGroup.pushLayer<TestingLayer>();
+  EXPECT_EQ(layerGroup.size(), 2);
+
   layerGroup.pushOverlay<TestingLayer>();
+  EXPECT_EQ(layerGroup.size(), 3);
+
   layerGroup.pushOverlay<TestingLayer>();
 
   EXPECT_EQ(layerGroup.size(), 4);
@@ -30,5 +36,118 @@ TEST(LayerGroupTest, PushLayers) {
       dynamic_cast<TestingLayer *>((layerGroup.end() - 2)->get())->detached);
   EXPECT_FALSE(
       dynamic_cast<TestingLayer *>((layerGroup.end() - 1)->get())->detached);
+}
+
+TEST(LayerGroupTest, PopLayers) {
+  LayerGroup layerGroup;
+  layerGroup.pushLayer<TestingLayer>();
+  layerGroup.pushLayer<TestingLayer>();
+  layerGroup.pushOverlay<TestingLayer>();
+  layerGroup.pushOverlay<TestingLayer>();
+
+  EXPECT_EQ(layerGroup.size(), 4);
+
+  EXPECT_FALSE(
+      dynamic_cast<TestingLayer *>(layerGroup.begin()->get())->detached);
+  EXPECT_FALSE(
+      dynamic_cast<TestingLayer *>((layerGroup.begin() + 1)->get())->detached);
+  EXPECT_FALSE(
+      dynamic_cast<TestingLayer *>((layerGroup.end() - 2)->get())->detached);
+  EXPECT_FALSE(
+      dynamic_cast<TestingLayer *>((layerGroup.end() - 1)->get())->detached);
+
+  auto layer1 = layerGroup.popLayer(0);
+  EXPECT_EQ(layerGroup.size(), 3);
+  EXPECT_TRUE(dynamic_cast<TestingLayer *>(layer1.get())->detached);
+
+  auto layer2 = layerGroup.popLayer(0);
+  EXPECT_EQ(layerGroup.size(), 2);
+  EXPECT_TRUE(dynamic_cast<TestingLayer *>(layer2.get())->detached);
+
+  auto overlay1 = layerGroup.popOverlay(1);
+  EXPECT_EQ(layerGroup.size(), 1);
+  EXPECT_TRUE(dynamic_cast<TestingLayer *>(overlay1.get())->detached);
+
+  auto overlay2 = layerGroup.popOverlay(0);
+  EXPECT_EQ(layerGroup.size(), 0);
+  EXPECT_TRUE(dynamic_cast<TestingLayer *>(overlay2.get())->detached);
+}
+
+TEST(LayerGroupTest, DisableAndEnableLayers) {
+  LayerGroup layerGroup;
+  layerGroup.pushLayer<TestingLayer>();
+  layerGroup.pushOverlay<TestingLayer>();
+
+  EXPECT_EQ(layerGroup.size(), 2);
+  EXPECT_TRUE(layerGroup.begin()->get()->isEnabled());
+  EXPECT_TRUE((layerGroup.begin() + 1)->get()->isEnabled());
+  EXPECT_FALSE(
+      dynamic_cast<TestingLayer *>((layerGroup.begin())->get())->detached);
+  EXPECT_TRUE(
+      dynamic_cast<TestingLayer *>((layerGroup.begin())->get())->attached);
+  EXPECT_FALSE(
+      dynamic_cast<TestingLayer *>((layerGroup.begin())->get())->updated);
+  EXPECT_FALSE(
+      dynamic_cast<TestingLayer *>((layerGroup.begin() + 1)->get())->detached);
+  EXPECT_TRUE(
+      dynamic_cast<TestingLayer *>((layerGroup.begin() + 1)->get())->attached);
+  EXPECT_FALSE(
+      dynamic_cast<TestingLayer *>((layerGroup.begin() + 1)->get())->updated);
+
+  layerGroup.update(0.16);
+  EXPECT_TRUE(
+      dynamic_cast<TestingLayer *>((layerGroup.begin())->get())->updated);
+  EXPECT_TRUE(
+      dynamic_cast<TestingLayer *>((layerGroup.begin() + 1)->get())->updated);
+
+  // One disabled
+  dynamic_cast<TestingLayer *>((layerGroup.begin())->get())->updated = false;
+  dynamic_cast<TestingLayer *>((layerGroup.begin() + 1)->get())->updated =
+      false;
+
+  (*layerGroup.begin())->disable();
+  EXPECT_FALSE((*layerGroup.begin())->isEnabled());
+  EXPECT_TRUE((*(layerGroup.begin() + 1))->isEnabled());
+
+  layerGroup.update(0.16);
+
+  EXPECT_FALSE(
+      dynamic_cast<TestingLayer *>((layerGroup.begin())->get())->updated);
+  EXPECT_TRUE(
+      dynamic_cast<TestingLayer *>((layerGroup.begin() + 1)->get())->updated);
+
+  // Both disabled
+  dynamic_cast<TestingLayer *>((layerGroup.begin())->get())->updated = false;
+  dynamic_cast<TestingLayer *>((layerGroup.begin() + 1)->get())->updated =
+      false;
+
+  (*(layerGroup.begin() + 1))->disable();
+  EXPECT_FALSE((*layerGroup.begin())->isEnabled());
+  EXPECT_FALSE((*(layerGroup.begin() + 1))->isEnabled());
+
+  layerGroup.update(0.16);
+
+  EXPECT_FALSE(
+      dynamic_cast<TestingLayer *>((layerGroup.begin())->get())->updated);
+  EXPECT_FALSE(
+      dynamic_cast<TestingLayer *>((layerGroup.begin() + 1)->get())->updated);
+
+  // Both enabled again
+  dynamic_cast<TestingLayer *>((layerGroup.begin())->get())->updated = false;
+  dynamic_cast<TestingLayer *>((layerGroup.begin() + 1)->get())->updated =
+      false;
+
+  for (auto &layer : layerGroup) {
+    layer->enable();
+  }
+  EXPECT_TRUE((*layerGroup.begin())->isEnabled());
+  EXPECT_TRUE((*(layerGroup.begin() + 1))->isEnabled());
+
+  layerGroup.update(0.16);
+
+  EXPECT_TRUE(
+      dynamic_cast<TestingLayer *>((layerGroup.begin())->get())->updated);
+  EXPECT_TRUE(
+      dynamic_cast<TestingLayer *>((layerGroup.begin() + 1)->get())->updated);
 }
 }  // namespace Neat
